4ffe9c0f1afa0979ddd9327eaeda0e9f7518d940
devgfe <gabriele.ferrero.2003@gmail.com>
2026-06-18 15:13:40 +0200

    feat(server): update database manager

--- server/database/database.js
+++ server/database/database.js
@@ -1,6 +1,7 @@
 import sqlite from "sqlite3";
+import { DB_FILE_PATH } from "../config/config.js";
 
-export const db = new sqlite.Database('../data/database.sqlite', (err) => {
+export const db = new sqlite.Database(DB_FILE_PATH, (err) => {
     if (err) {
         console.error("Error opening the database: ", err.message);
     } else {
